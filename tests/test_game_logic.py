50528af66e1624102c39d66c1eb0978452700534
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2024-12-27 15:19:32 -0500

    refactor: remove unused imports from test_game_logic

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from src import MantisGame, MatcherBot, ScorerBot, ThiefBot, RandomBot, CertaintyBot
+from src import MantisGame
 
 
 def test_reset():
