1f446fe37ddf267fcfbd2ddc33bed74e9c0d7a2b
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2025-03-04 14:08:59 -0500

    refactor(tests): refactor card initialization in tests using Sample_Cards class

--- tests/test_mantis_logic.py
+++ tests/test_mantis_logic.py
@@ -1,6 +1,17 @@
 import pytest
 import mantis_logic
 
+class Sample_Cards:
+    def __init__(self):
+        game = mantis_logic.Mantis()
+        self.red_card = game.Card()
+        self.red_card.colour = "red"
+        self.green_card = game.Card()
+        self.green_card.colour = "green"
+        self.blue_card = game.Card()
+        self.blue_card.colour = "blue"
+        self.blue_card.possible_colours = ["blue", "orange", "yellow"]
+
 def test_convert_colour_index_to_name():
     assert mantis_logic.convert_colour_index_to_name(1) == "red"
     assert mantis_logic.convert_colour_index_to_name(2) == "orange"
@@ -43,6 +54,7 @@ def test_is_valid_name():
 
 def test_score_action():
     game = mantis_logic.Mantis()
+    sample_cards = Sample_Cards()
     assert len(game.deck) == 0
 
     p1 = game.Player(game, None, "Player 1")
@@ -50,13 +62,9 @@ def test_score_action():
     assert p1.tank == []
     assert p1.score_pile == []
 
-    red_card = game.Card()
-    red_card.colour = "red"
-    green_card = game.Card()
-    green_card.colour = "green"
-    game.deck = [red_card, green_card]
+    game.deck = [sample_cards.red_card, sample_cards.green_card]
 
-    p1.tank = [red_card]
+    p1.tank = [sample_cards.red_card]
 
     assert game.deck[-1].colour == "green"
     assert game.deck[-2].colour == "red"
@@ -76,6 +84,7 @@ def test_score_action():
 
 def test_steal_action():
     game = mantis_logic.Mantis()
+    sample_cards = Sample_Cards()
     assert len(game.deck) == 0
 
     p1 = game.Player(game, None, "Player 1")
@@ -86,14 +95,10 @@ def test_steal_action():
     assert p2.tank == []
     assert p2.score_pile == []
 
-    red_card = game.Card()
-    red_card.colour = "red"
-    green_card = game.Card()
-    green_card.colour = "green"
-    game.deck = [red_card, green_card]
+    game.deck = [sample_cards.red_card, sample_cards.green_card]
 
-    p1.tank = [green_card]
-    p2.tank = [red_card]
+    p1.tank = [sample_cards.green_card]
+    p2.tank = [sample_cards.red_card]
 
     # Unsucesfully steal, giving the drawn green card to p2's tank
     p1.steal_action(p2)
@@ -111,21 +116,14 @@ def test_steal_action():
 
 def test_get_info():
     game = mantis_logic.Mantis()
+    sample_cards = Sample_Cards()
     p1 = game.Player(game, None, "Player 1")
     p2 = game.Player(game, None, "Player 2")
 
-    red_card = game.Card()
-    red_card.colour = "red"
-    green_card = game.Card()
-    green_card.colour = "green"
-    blue_card = game.Card()
-    blue_card.colour = "blue"
-    blue_card.possible_colours = ["blue", "orange", "yellow"]
-
-    game.deck = [blue_card]
-    p1.tank = [green_card, green_card]
-    p2.tank = [red_card, red_card]
-    p2.score_pile = [green_card]
+    game.deck = [sample_cards.blue_card]
+    p1.tank = [sample_cards.green_card, sample_cards.green_card]
+    p2.tank = [sample_cards.red_card, sample_cards.red_card]
+    p2.score_pile = [sample_cards.green_card]
 
     info = game.get_info()
 
