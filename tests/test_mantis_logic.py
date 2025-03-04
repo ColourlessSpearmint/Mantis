90a8f1dd7d6512978552215596d43579ab1b7fca
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2025-03-04 09:30:24 -0500

    style(tests): update test function names to snake_case

--- tests/test_mantis_logic.py
+++ tests/test_mantis_logic.py
@@ -1,30 +1,33 @@
+import pytest
 import mantis_logic
 
-def test_convert_to_colour_name():
-    assert mantis_logic.convertColourIndexToName(1) == "red"
-    assert mantis_logic.convertColourIndexToName(2) == "orange"
-    assert mantis_logic.convertColourIndexToName(3) == "yellow"
-    assert mantis_logic.convertColourIndexToName(4) == "green"
-    assert mantis_logic.convertColourIndexToName(5) == "blue"
-    assert mantis_logic.convertColourIndexToName(6) == "purple"
-    assert mantis_logic.convertColourIndexToName(7) == "pink"
+def test_convert_colour_index_to_name():
+    assert mantis_logic.convert_colour_index_to_name(1) == "red"
+    assert mantis_logic.convert_colour_index_to_name(2) == "orange"
+    assert mantis_logic.convert_colour_index_to_name(3) == "yellow"
+    assert mantis_logic.convert_colour_index_to_name(4) == "green"
+    assert mantis_logic.convert_colour_index_to_name(5) == "blue"
+    assert mantis_logic.convert_colour_index_to_name(6) == "purple"
+    assert mantis_logic.convert_colour_index_to_name(7) == "pink"
 
-    assert mantis_logic.convertColourListToNames([1,2,3]) == ["red","orange","yellow"]
-    assert mantis_logic.convertColourListToNames([4,5,6]) == ["green","blue","purple"]
-    assert mantis_logic.convertColourListToNames([3,5,7]) == ["yellow","blue","pink"]
+def test_convert_colour_list_to_names():
+    assert mantis_logic.convert_colour_list_to_names([1,2,3]) == ["red","orange","yellow"]
+    assert mantis_logic.convert_colour_list_to_names([4,5,6]) == ["green","blue","purple"]
+    assert mantis_logic.convert_colour_list_to_names([3,5,7]) == ["yellow","blue","pink"]
 
 def test_validate_colour():
-    assert mantis_logic.validateColour("red") is True
-    assert mantis_logic.validateColour("orange") is True
-    assert mantis_logic.validateColour("yellow") is True
-    assert mantis_logic.validateColour("green") is True
-    assert mantis_logic.validateColour("blue") is True
-    assert mantis_logic.validateColour("purple") is True
-    assert mantis_logic.validateColour("pink") is True
+    assert mantis_logic.validate_colour("red") is True
+    assert mantis_logic.validate_colour("orange") is True
+    assert mantis_logic.validate_colour("yellow") is True
+    assert mantis_logic.validate_colour("green") is True
+    assert mantis_logic.validate_colour("blue") is True
+    assert mantis_logic.validate_colour("purple") is True
+    assert mantis_logic.validate_colour("pink") is True
 
-    assert mantis_logic.validateColour("1") is False
+    assert mantis_logic.validate_colour("1") is False
+
 def test_card_generation():
     game = mantis_logic.Mantis()
-    for i in range(100):
+    for _ in range(100):
         card = game.Card()
-        assert card.colour in card.possibleColours
+        assert card.colour in card.possible_colours
